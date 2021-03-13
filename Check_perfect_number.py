929f808234ac5bc51683c0558fbc342cc595065c
Prajjwal Walchale <64081351+Prajjwal-Walchale@users.noreply.github.com>
2021-03-13 14:03:33 +0530

    Create Check_perfect_number.py

---
+++ Check_perfect_number.py
@@ -0,0 +1,40 @@
+#Problem Statement
+#Write a python function to check whether the given number is a perfect number or not. The function should returns true if the number is a perfect number, else it should returns false. Hint: Perfect number is a positive whole number that is equal to the sum of its proper divisors.
+#The first perfect number is 6 as the sum of its proper positive divisors, 1,2 and 3 is 6. Other perfect
+#numbers are 28, 496, 8128 ...
+#Extend the program written for the above problem to write another function to find all perfect numbers in a given list of numbers. Populate the perfect numbers in a list and return the list. If no perfect numbers found, return an empty list.
+#Note- Reuse the code wherever possible.
+
+
+
+
+def check_perfect_number(number):
+    #start writing your code here
+    sum_of_divisor=0
+    for i in range(1,number):
+        if(number%i==0):
+            
+            sum_of_divisor+=i
+            if(sum_of_divisor==number):
+                return True
+            
+    
+    else:
+        return False
+
+def check_perfectno_from_list(no_list):
+    #start writing your code here
+    list1=[]
+    sum_of_divisor=0
+    for i in range(len(no_list)):
+    
+        
+            perfect=check_perfect_number(no_list[i])
+            if(perfect==True):
+                list1.append(no_list[i])
+            
+                
+    return list1       
+
+perfectno_list=check_perfectno_from_list([18,6,4,2,1,28])
+print(perfectno_list)
